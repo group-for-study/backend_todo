4bc785241e34981077c5653e5b8558c7fa7ce6a4
ne-eun <p.tyche92@gmail.com>
2024-05-12 16:15:45 +0900

    feat: atlas 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [MongooseModule.forRoot('mongodb://localhost/nest'), TodoModule],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		MongooseModule.forRoot(process.env.DATABASE_URI),
+		TodoModule,
+	],
 })
 export class AppModule {}
